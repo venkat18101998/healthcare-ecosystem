aa3cc58e0da6084072f9294972e55613937df9b1
Rajesh9976 <rajeshrs9986@gmail.com>
2024-03-08 20:10:47 +0530

    changes

--- src/components/multiSelectDropdown/MultiSelectDropdown.js
+++ src/components/multiSelectDropdown/MultiSelectDropdown.js
@@ -6,18 +6,11 @@ import { Container, Form, Card, Row, Col, Spinner } from 'react-bootstrap';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-
 const MultiSelectDropdown = () => {
-
     const [selectedItems, setSelectedItems] = useState([]);
     const [selectedDiseases, setSelectedDiseases] = useState([]);
-    const [drugList, setDrugList] = useState([]);
-    const [drugListError, setDrugListError] = useState([]);
+    const [diseaseData, setDiseaseData] = useState([]);
     const [listLoading, setListLoading] = useState(false);
-
-
-
-    console.log(drugListError, "---");
 
     const symptomsArray = symptomsText.split('\n');
 
@@ -32,17 +25,11 @@ const MultiSelectDropdown = () => {
     }));
 
     const getMatchingDiseases = () => {
-
         const selectedSymptoms = selectedItems.map(item => item.value.trim());
-        const matchingDiseases = medicalData.filter(disease => {
+        return medicalData.filter(disease => {
             return selectedSymptoms.every(symptom => disease.symptoms.includes(symptom));
         });
-
-        return matchingDiseases.map(disease => disease.disease);
     };
-
-    const matchingDiseases = getMatchingDiseases();
-    let uniquematchingDiseases = [...new Set(matchingDiseases)];
 
     const handleChange = selectedOptions => {
         setSelectedItems(selectedOptions);
@@ -63,29 +50,21 @@ const MultiSelectDropdown = () => {
                 setListLoading(true);
                 const response = await axios.get(`https://candidate-assignment-5hohk5qryq-as.a.run.app/getDrugs/${disease}`);
                 setListLoading(false);
-                return response.data;
+                return { disease, drugs: response.data };
             } catch (error) {
-                setDrugListError(error.response?.data.detail)
                 toast.error(`Error fetching drug list for ${disease}: ${error.response?.data.detail}`);
                 setListLoading(false);
-                return [];
+                return { disease, drugs: [] };
             }
         };
 
-        const fetchDrugLists = async () => {
-            const drugLists = await Promise.all(selectedDiseases.map(disease => fetchDrugList(disease)));
-            setDrugList(drugLists.flat());
+        const fetchDataForSelectedDiseases = async () => {
+            const diseaseData = await Promise.all(selectedDiseases.map(disease => fetchDrugList(disease)));
+            setDiseaseData(diseaseData);
         };
 
-        if (selectedDiseases.length > 0) {
-            fetchDrugLists();
-        }
-        else {
-            setDrugList([]);
-        }
+        fetchDataForSelectedDiseases();
     }, [selectedDiseases]);
-
-
 
     return (
         <React.Fragment>
@@ -125,30 +104,26 @@ const MultiSelectDropdown = () => {
                                         <Card.Title>Matching Diseases</Card.Title>
                                         <ul>
                                             <Form>
-                                                {uniquematchingDiseases.length > 0 ? (
-                                                    uniquematchingDiseases.map(disease => (
-                                                        <Form.Check
-                                                            key={disease}
-                                                            type="checkbox"
-                                                            id={disease}
-                                                            label={disease}
-                                                            onChange={handleDiseaseChange}
-                                                        />
-                                                    ))
-                                                ) : (
-                                                    <h2 className='mt-3 matchingDiseases'>No matching diseases found</h2>
-                                                )}
+                                                {getMatchingDiseases().map(disease => (
+                                                    <Form.Check
+                                                        key={disease.disease}
+                                                        type="checkbox"
+                                                        id={disease.disease}
+                                                        label={disease.disease}
+                                                        onChange={handleDiseaseChange}
+                                                    />
+                                                ))}
                                             </Form>
                                         </ul>
                                     </Card.Body>
                                 </Card>
                             </Col>
                         )}
-                        {drugList.length > 0 && (
-                            <Col md={4}>
+                        {diseaseData.map(({ disease, drugs }) => (
+                            <Col md={4} key={disease}>
                                 <Card className='mt-5'>
                                     <Card.Body>
-                                        <Card.Title>Drug List</Card.Title>
+                                        <Card.Title>{disease}</Card.Title>
                                         {listLoading ? (
                                             <>
                                                 <div className='d-flex align-items-center mt-3'>
@@ -158,19 +133,18 @@ const MultiSelectDropdown = () => {
                                             </>
                                         ) : (
                                             <ul>
-                                                {drugList.map(drug => (
-                                                    <li key={drug}>{drug}</li>
+                                                {drugs.map((drug, index) => (
+                                                    <li key={index}>{drug}</li>
                                                 ))}
                                             </ul>
                                         )}
                                     </Card.Body>
                                 </Card>
                             </Col>
-                        )}
+                        ))}
                     </Row>
                 </div>
             </Container>
-
         </React.Fragment>
     );
 };
